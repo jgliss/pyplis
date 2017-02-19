87772a69d9b6c8986ef1e9f0321e5aec535972f1
jgliss <jg@nilu.no>
2017-02-19 20:18:04 +0100

    RTD build failed: mocked out astropy from docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ class Mock(MagicMock):
 
 MOCK_MODULES = [
     'mpl_toolkits.basemap',
-    'astropy.io'
+    'astropy.io',
+    'astropy'
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
