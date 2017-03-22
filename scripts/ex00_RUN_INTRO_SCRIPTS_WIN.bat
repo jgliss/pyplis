5df14f783cb652775788f67480b33bd1104fdb8e
Jonas <jg@nilu.no>
2017-03-22 11:31:14 +0100

    Delete ex00_RUN_INTRO_SCRIPTS_WIN.bat

--- scripts/ex00_RUN_INTRO_SCRIPTS_WIN.bat
+++
@@ -1,6 +0,0 @@
-C:\Python27\python.exe ex0_1_img_handling.py
-C:\Python27\python.exe ex0_2_camera_setup.py
-C:\Python27\python.exe ex0_3_imglists_manually.py
-C:\Python27\python.exe ex0_4_imglists_auto.py
-C:\Python27\python.exe ex0_6_pcs_lines.py
-C:\Python27\python.exe ex0_7_cellcalib_manual.py
