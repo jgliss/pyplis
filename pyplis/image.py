ca7f85e7052887bfbb168557ce1bd350bcdc61a2
solvejg <asd@nilu.no>
2017-09-20 14:54:34 +0200

    Added 3 custom meta data to image class

--- pyplis/image.py
+++ pyplis/image.py
@@ -137,7 +137,10 @@ class Img(object):
                         ("device_id"     ,   ""),
                         ("ser_no"        ,   ""),
                         ("temperature"   ,   float(0.0)),   # temperature in Celsius
-                        ("img_idx"       ,   int(0))])
+                        ("img_idx"       ,   int(0)),    # index in multi-img file formats as *.fits
+                        ("custom1"       ,   nan),  # 3 dummy attributes which can be used for custom defined parameters
+                        ("custom2"       ,   nan),
+                        ("custom3"       ,   nan)])
                         
         try:
             data, meta_info = import_method(input, meta_info) 
