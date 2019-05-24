b13093f5f74ebabd9ed179c010e7f0a06a247147
jgliss <jonasgliss@gmail.com>
2019-05-24 12:37:23 +0200

    Added 2 default loggers "logger" and "print_log" in __init__.py and cleaned up imports, etc.

--- pyplis/__init__.py
+++ pyplis/__init__.py
@@ -17,34 +17,70 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 """Package initialisation."""
 from __future__ import (absolute_import, division)
-from os.path import abspath, dirname
-from pkg_resources import get_distribution
-from matplotlib import rcParams
 
-rcParams["mathtext.default"] = u"regular"
+def _init_supplemental():
+    from os.path import abspath, dirname
+    from pkg_resources import get_distribution
+    from matplotlib import rcParams
 
-__dir__ = abspath(dirname(__file__))
-__version__ = get_distribution('pyplis').version
+    rcParams["mathtext.default"] = u"regular"
+
+    return (get_distribution('pyplis').version, abspath(dirname(__file__)))
+
+def _init_logger():
+    import logging
+    ### LOGGING
+    # Note: configuration will be propagated to all child modules of
+    # pyplis, for details see
+    # http://eric.themoritzfamily.com/learning-python-logging.html
+    logger = logging.getLogger('pyplis')
+
+    default_formatter = logging.Formatter( \
+        "%(asctime)s:%(levelname)s: %(message)s")
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(default_formatter)
+
+    logger.addHandler(console_handler)
+
+    logger.setLevel(logging.CRITICAL)
+
+    print_log = logging.getLogger('pyplis_print')
+
+    print_handler = logging.StreamHandler()
+    print_handler.setFormatter(logging.Formatter("%(message)s"))
+
+    print_log.addHandler(print_handler)
+
+    print_log.setLevel(logging.INFO)
+    return (logger, print_log)
+
+def check_requirements():
+    try:
+        import pydoas
+        PYDOASAVAILABLE = True
+    except BaseException:
+        PYDOASAVAILABLE = False
+    try:
+        import geonum
+        GEONUMAVAILABLE = True
+    except BaseException:
+        GEONUMAVAILABLE = False
+    try:
+        from PIL.Image import open as open_pil
+        PILAVAILABLE = True
+    except BaseException:
+        PILAVAILABLE = False
+    return (PYDOASAVAILABLE, GEONUMAVAILABLE, PILAVAILABLE)
+
+PYDOASAVAILABLE, GEONUMAVAILABLE, PILAVAILABLE = check_requirements()
+logger, print_log = _init_logger()
+
+__version__, __dir__ = _init_supplemental()
 _LIBDIR = __dir__  # from older version
-# =============================================================================
-#
-# URL_TESTDATA = ("https://folk.nilu.no/~gliss/pyplis_testdata/"
-#                 "pyplis_etna_testdata.zip")
-# =============================================================================
 
 URL_TESTDATA = ("https://folk.nilu.no/~arve/pyplis/"
                 "pyplis_etna_testdata.zip")
-try:
-    import pydoas
-    PYDOASAVAILABLE = True
-except BaseException:
-    PYDOASAVAILABLE = False
-
-try:
-    import geonum
-    GEONUMAVAILABLE = 1
-except BaseException:
-    GEONUMAVAILABLE = 0
 
 from .setupclasses import MeasSetup, Camera, Source
 from .geometry import MeasGeometry
@@ -66,12 +102,3 @@ from .fluxcalc import (EmissionRateAnalysis, EmissionRates,
 from .optimisation import PolySurfaceFit, MultiGaussFit
 from . import custom_image_import
 from .inout import download_test_data, find_test_data
-# ==============================================================================
-# import model_functions
-# import helpers
-# import exceptions
-# import glob
-# ==============================================================================
-
-
-# import gui_features as gui_features
