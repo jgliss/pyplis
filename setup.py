70c1ba626f5d087d082fc9e49cc51509aad9f37c
Jonas Gliss <jonasgliss@gmail.com>
2025-03-12 18:57:35 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -60,8 +60,10 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.,
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
     ],
     #packages    =   ['pyplis'],
     package_dir =   {'pyplis'     :    'pyplis'},
